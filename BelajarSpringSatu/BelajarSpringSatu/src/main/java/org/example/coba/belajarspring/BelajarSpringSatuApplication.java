3063d7aabc835c220283791a4b5e145702a8ab1b
leosantara <leonardo.bondan@ti.ukdw.ac.id>
2024-09-11 12:17:31 +0700

    Integrasi Maven, Java, SpringBoot

---
+++ BelajarSpringSatu/BelajarSpringSatu/src/main/java/org/example/coba/belajarspring/BelajarSpringSatuApplication.java
@@ -0,0 +1,13 @@
+package org.example.coba.belajarspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringSatuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelajarSpringSatuApplication.class, args);
+    }
+
+}
---
+++ BelajarSpringSatu/BelajarSpringSatu/src/test/java/org/example/coba/belajarspring/BelajarSpringSatuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.coba.belajarspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringSatuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
